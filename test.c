94dceb8b27a59c1cbcc6a39a6262b08418259786
Spudoku <calebisntshort@gmail.com>
2026-01-24 14:19:20 -0800

    Working on editing character map 60 minutes

--- test.c
+++ test.c
@@ -11,20 +11,28 @@
 
 // #define SCREEN_HEIGHT 24
 
-// these addresses were from Superbug
-#define SCREEN_ADDR   ((unsigned char*)0xB400)
 
 // Memory locations
 #define DISPLAY_LIST 0x230    // location that the display list must be pushed to
+#define CHARSET_PTR 0x2F4     // character base
 
+#define CHARSET_ADDR 0x3C00
+// according to Ed, 0x3C00 is chosen since its 4 pages from the screen memory buffer he allocated (0x4000). S
+// According to Mapping the Atari, cannot set 756 to any odd number or else we will have screen garbage.
+// additionally, need to start on a page boundary (any value in the form $XX00), such as $C000
+// DL_CHR40x8x1
 
+//  #define CHARSET_ADDR // location of character set
 
 // function declarations
 void fix_displayList();
-unsigned char ScreenMemory[760];
+void init_charset();
 
-// variable declarations
-int i;
+// memory allocations
+unsigned char ScreenMemory[760];
+unsigned char CharMap[1024];     // allocate 1024 bytes for character set
+
+
 
 char DisplayList[] = {
     // 24 blank lines
@@ -33,6 +41,7 @@ char DisplayList[] = {
     DL_BLK8,
     // TODO: tell ANTIC to load Graphics 2 at Screen Address, total 24 times
     DL_LMS(DL_GRAPHICS2),
+    // was DL_GRAPHICS2, now DL_GRAPHICS0
     // these 2 bytes will store the location of the screen memory
     0x00,0x00,
     DL_GRAPHICS2,DL_GRAPHICS2,DL_GRAPHICS2,DL_GRAPHICS2,DL_GRAPHICS2,DL_GRAPHICS2,
@@ -44,12 +53,16 @@ char DisplayList[] = {
     0x00,0x00
     };
 
+    // variable declarations
+int i;
+int charStart = 0;
+
 int main() {
    fix_displayList();
+    init_charset();
 
-
-    for (i = 33; i < 33 + 26; i++) {
- ScreenMemory[i - 33] = i;
+    for (i = charStart; i < charStart + 5; i++) {
+ ScreenMemory[i - charStart] = 0;
     }
    
     while (true) {
@@ -71,6 +84,13 @@ void fix_displayList() {
    DisplayList[29] = (dl_addr >> 8) & 0xFF; // this injects the 8 least signifcant bits
    POKEW(DISPLAY_LIST,dl_addr);
     // return dl_addr;
+}
+
+void init_charset() {
+    // address of CharMap
+    // unsigned int charmap_addr = (unsigned int )CharMap;
+      unsigned int charmap_addr = (unsigned int )0x3C;
+    POKE(CHARSET_PTR,charmap_addr);
 }
 
 
