4f70c7c40cb73cef653fe029cc92147256ca384b
suspiciouslyBee <suspiciously@bee.movie>
2026-02-13 16:29:38 -0800

    added unsigned support to vector 2

--- util_structs.h
+++ util_structs.h
@@ -1,17 +1,24 @@
 #ifndef UTIL_S_H
 #define UTIL_S_H
 
-typedef struct Vector2 {
+typedef struct s32Vector2 {
   int x;
   int y;
 } Vector2;
 
 //Useful for certain values that shouldn't be huge (i.e. velocity)
 //or even screen coords
-typedef struct u8Vector2{
+typedef struct u16Vector2{
   unsigned char x;
   unsigned char y;
 } u8Vector2;
+
+
+//used for global map coords
+typedef struct u32Vector2 {
+  unsigned int x;
+  unsigned int y;
+} u32Vector2;
 
 
 typedef enum STATUS {
